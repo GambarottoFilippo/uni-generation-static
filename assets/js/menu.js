97d84a7ce652fbe030a1e564b1bf85efe6a91e9a
GambarottoFilippo <gambarotto.filippo@gmail.com>
2019-11-08 23:53:45 +0100

    Fix healthcare

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -9,3 +9,18 @@ $('html').not($('.side-menu')).on('click', function(e) {
 	if (e.target.classList.contains('side-menu')) return;
 	$('.side-menu-bg').removeClass('side-menu-open');
 });
+
+if (window.scrollY === 0) {
+  $('.topbar-desktop__container').removeClass('topbar-desktop__container-scrolled');
+} else {
+  $('.topbar-desktop__container').addClass('topbar-desktop__container-scrolled');
+}
+
+window.addEventListener('scroll', () => {
+  let windowY = window.scrollY
+  if (windowY === 0) {
+    $('.topbar-desktop__container').removeClass('topbar-desktop__container-scrolled');
+  } else {
+    $('.topbar-desktop__container').addClass('topbar-desktop__container-scrolled');
+  }
+})
